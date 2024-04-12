da1b961fb17de2fa4681a7d12f1e76b611ffec09
Neson55 <1385463@mail.ru>
2024-04-12 15:31:21 +0400

    add post+ all buttons him

--- src/pages/current-post/index.tsx
+++ src/pages/current-post/index.tsx
@@ -1,6 +1,8 @@
 import React from 'react'
+import { useParams } from 'react-router-dom'
 
 export const CurrentPost = () => {
+  const  params = useParams<{id: string}>()
   return (
     <div>CurrentPost</div>
   )
